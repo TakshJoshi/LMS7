e876603285c114b3f1a8aba01cfd9d11b52546c3
rakshit2309 <rakshitpanjeta23@gmail.com>
2025-02-16 21:56:21 +0530

    Initial Commit

---
+++ Team7LibraryManagementSystem/Team7LibraryManagementSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  Team7LibraryManagementSystemApp.swift
+//  Team7LibraryManagementSystem
+//
+//  Created by Rakshit  on 16/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Team7LibraryManagementSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
